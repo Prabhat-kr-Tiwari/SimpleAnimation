256a59fc5c38ba14520e1a1b4634e9e0913c73a3
pr847 <Sonukbc6@gmail.com>
2023-02-23 10:19:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/simpleanimation/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.simpleanimation
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.simpleanimation.ui.theme.SimpleAnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var sizeState by remember{mutableStateOf(200.dp)}
+            val size by animateDpAsState(targetValue = sizeState,
+                tween(
+                    durationMillis = 1000
+                )
+                /*keyframes {
+                    durationMillis=5000
+                    sizeState at 0 with LinearOutSlowInEasing
+                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                    sizeState * 2f at 5000
+                }*/
+                /*spring(
+                    Spring.DampingRatioHighBouncy
+                )*/
+            /*tween(
+                durationMillis=3000,
+                delayMillis=300,
+
+                easing= LinearOutSlowInEasing
+            )*/
+            
+
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = androidx.compose.ui.graphics.Color.Red,
+                targetValue = androidx.compose.ui.graphics.Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                repeatMode = RepeatMode.Reverse
+
+                )
+            )
+
+            Box(modifier= Modifier
+                .size(size)
+                .background(color),
+                contentAlignment= Alignment.Center){
+                Button(onClick={
+                    sizeState+=50.dp
+
+                }){
+                    Text("Increase size ")
+
+                }
+
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SimpleAnimationTheme {
+        Greeting("Android")
+    }
+}
